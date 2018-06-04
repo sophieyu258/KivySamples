624babf9f2345652256b600d1e1b183a1571ac7f
bhang <bhang@yahoo.com>
2018-06-04 14:53:27 -0400

    label dynamic content

--- TkinterTutorial/TkinterTutorial.py
+++ TkinterTutorial/TkinterTutorial.py
@@ -1,19 +1,20 @@
 import tkinter as tk
 
+counter = 0 
+def counter_label(label):
+  def count():
+    global counter
+    counter += 1
+    label.config(text=str(counter))
+    label.after(1000, count)
+  count()
+ 
+ 
 root = tk.Tk()
-tk.Label(root, 
-		 text="Red Text in Times Font",
-		 fg = "red",
-		 font = "Times").pack()
-tk.Label(root, 
-		 text="Green Text in Helvetica Font",
-		 fg = "light green",
-		 bg = "dark green",
-		 font = "Helvetica 16 bold italic").pack()
-tk.Label(root, 
-		 text="Blue Text in Verdana bold",
-		 fg = "blue",
-		 bg = "yellow",
-		 font = "Verdana 10 bold").pack()
-
+root.title("Counting Seconds")
+label = tk.Label(root, fg="green")
+label.pack()
+counter_label(label)
+button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+button.pack()
 root.mainloop()
