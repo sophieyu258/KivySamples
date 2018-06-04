47f2789ce0a416ee47f232ceb7608f5991096b7d
bhang <bhang@yahoo.com>
2018-06-04 14:44:44 -0400

    label image

--- TkinterTutorial/TkinterTutorial.py
+++ TkinterTutorial/TkinterTutorial.py
@@ -3,15 +3,23 @@ import tkinter as tk
 root = tk.Tk()
 logo = tk.PhotoImage(file="image.gif")
 
-w1 = tk.Label(root, image=logo).pack(side="right")
+# w1 = tk.Label(root, image=logo).pack(side="right")
 
 explanation = """At present, only GIF and PPM/PGM
 formats are supported, but an interface 
 exists to allow additional image file
 formats to be added easily."""
 
-w2 = tk.Label(root, 
-              justify=tk.LEFT,
-              padx = 10, 
-              text=explanation).pack(side="left")
+#w = tk.Label(root, 
+#             compound = tk.CENTER,
+#             text=explanation, 
+#             image=logo).pack(side="right")
+
+w = tk.Label(root, 
+          justify=tk.LEFT,
+          compound = tk.LEFT,
+          padx = 10, 
+          text=explanation, 
+          image=logo).pack(side="right")
+
 root.mainloop()
