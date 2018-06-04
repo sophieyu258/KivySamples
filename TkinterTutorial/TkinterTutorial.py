7ff447c8be3c664c872e9d3b51f922c558def5c2
bhang <bhang@yahoo.com>
2018-06-04 14:30:28 -0400

    show image

--- TkinterTutorial/TkinterTutorial.py
+++ TkinterTutorial/TkinterTutorial.py
@@ -1,13 +1,17 @@
-
 import tkinter as tk
 
-# if you are still working under a Python 2 version, 
-# comment out the previous line and uncomment the following line
-# import Tkinter as tk
-
 root = tk.Tk()
+logo = tk.PhotoImage(file="image.gif")
 
-w = tk.Label(root, text="Hello Tkinter!")
-w.pack()
+w1 = tk.Label(root, image=logo).pack(side="right")
 
+explanation = """At present, only GIF and PPM/PGM
+formats are supported, but an interface 
+exists to allow additional image file
+formats to be added easily."""
+
+w2 = tk.Label(root, 
+              justify=tk.LEFT,
+              padx = 10, 
+              text=explanation).pack(side="left")
 root.mainloop()
