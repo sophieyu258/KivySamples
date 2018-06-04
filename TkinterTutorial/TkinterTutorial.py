1d2333a5b67be49ab1dab3c6b10b78c74f785aa2
bhang <bhang@yahoo.com>
2018-06-04 16:03:02 -0400

    buttons

--- TkinterTutorial/TkinterTutorial.py
+++ TkinterTutorial/TkinterTutorial.py
@@ -1,20 +1,21 @@
 import tkinter as tk
+    
 
-counter = 0 
-def counter_label(label):
-  def count():
-    global counter
-    counter += 1
-    label.config(text=str(counter))
-    label.after(1000, count)
-  count()
- 
- 
+def write_slogan():
+    print("Tkinter is easy to use!")
+
 root = tk.Tk()
-root.title("Counting Seconds")
-label = tk.Label(root, fg="green")
-label.pack()
-counter_label(label)
-button = tk.Button(root, text='Stop', width=25, command=root.destroy)
-button.pack()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, 
+                   text="QUIT", 
+                   fg="red",
+                   command=quit)
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,
+                   text="Hello",
+                   command=write_slogan)
+slogan.pack(side=tk.LEFT)
+
 root.mainloop()
