508ee121a122285ad4beba3f60c5806f094dd240
bhang <bhang@yahoo.com>
2018-06-05 14:19:13 -0400

    Painting Interactively into a Canvas

--- TkinterTutorial/TkinterTutorial.py
+++ TkinterTutorial/TkinterTutorial.py
@@ -1,23 +1,23 @@
 from tkinter import *
 
-def show_entry_fields():
-   print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
-   e1.delete(0,END)
-   e2.delete(0,END)
+canvas_width = 500
+canvas_height = 150
+
+def paint( event ):
+   python_green = "#476042"
+   x1, y1 = ( event.x - 1 ), ( event.y - 1 )
+   x2, y2 = ( event.x + 1 ), ( event.y + 1 )
+   w.create_oval( x1, y1, x2, y2, fill = python_green )
 
 master = Tk()
-Label(master, text="First Name").grid(row=0)
-Label(master, text="Last Name").grid(row=1)
+master.title( "Painting using Ovals" )
+w = Canvas(master, 
+           width=canvas_width, 
+           height=canvas_height)
+w.pack(expand = YES, fill = BOTH)
+w.bind( "<B1-Motion>", paint )
 
-e1 = Entry(master)
-e2 = Entry(master)
-e1.insert(10,"Miller")
-e2.insert(10,"Jill")
-
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
-
-Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
-Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
-
-mainloop( )
+message = Label( master, text = "Press and Drag the mouse to draw" )
+message.pack( side = BOTTOM )
+    
+mainloop()
