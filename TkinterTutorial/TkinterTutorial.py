a3b91c9566bd1f31c239519e747ab8d1e8154f0d
bhang <bhang@yahoo.com>
2018-06-07 18:25:29 -0400

    Place Geometry Manager

--- TkinterTutorial/TkinterTutorial.py
+++ TkinterTutorial/TkinterTutorial.py
@@ -1,33 +1,27 @@
-from tkinter import *
-from tkinter.filedialog import *
-from tkinter.colorchooser import askcolor                  
 
-def colorcallback():
-    result = askcolor(color="#6A9662", 
-                      title = "Bernd's Colour Chooser") 
-    print(result)
+# https://www.python-course.eu/tkinter_layout_management.php
+
+import tkinter as tk
+import random
     
-root = Tk()
+root = tk.Tk()
+# width x height + x_offset + y_offset:
+root.geometry("170x250+30+230") 
+     
+languages = ['Python','Perl','C++','Java','Tcl/Tk']
+labels = range(5)
+for i in range(5):
+   ct = [random.randrange(256) for x in range(3)]
+   brightness = int(round(0.299*ct[0] + 0.587*ct[1] + 0.114*ct[2]))
+   ct_hex = "%02x%02x%02x" % tuple(ct)
+   bg_colour = '#' + "".join(ct_hex)
+   l = tk.Label(root, 
+                text=languages[i], 
+                fg='White' if brightness < 120 else 'Black', 
+                bg=bg_colour)
+   l.place(x = 20, y = 30 + i*30, width=120, height=25)
+   
+w = tk.Label(root, text="green", bg="green", fg="black")
+w.place(x = 10, y = 200, width=120, height=25)
 
-def filecallback():
-    name= askopenfilename() 
-    print(name)
-    
-def dircallback():
-    name= askdirectory() 
-    print(name)
-
-Button(root, text='File Open', command=filecallback).pack(fill=X)
-
-Button(root, text='Directory Open', command=dircallback).pack(fill=X)
-
-Button(root, 
-       text='Choose Color', 
-       fg="darkgreen", 
-       command=colorcallback).pack(fill=X)
-
-Button(text='Quit', 
-       command=root.quit,
-       fg="red").pack(fill=X)
-
-mainloop()
+root.mainloop()
