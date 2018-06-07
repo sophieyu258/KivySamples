d69768f47e722a584b5a184734b555eea04a84e5
bhang <bhang@yahoo.com>
2018-06-07 16:51:50 -0400

    file choose dialog; color choose dialog

--- TkinterTutorial/TkinterTutorial.py
+++ TkinterTutorial/TkinterTutorial.py
@@ -1,15 +1,27 @@
 from tkinter import *
-from tkinter.messagebox import *
+from tkinter.filedialog import *
+from tkinter.colorchooser import askcolor                  
 
-def answer():
-    showerror("Answer", "Sorry, no answer available")
+def colorcallback():
+    result = askcolor(color="#6A9662", 
+                      title = "Bernd's Colour Chooser") 
+    print(result)
+    
+root = Tk()
 
-def callback():
-    if askyesno('Verify', 'Really quit?'):
-        showwarning('Yes', 'Not yet implemented')
-    else:
-        showinfo('No', 'Quit has been cancelled')
+def filecallback():
+    name= askopenfilename() 
+    print(name)
+    
+Button(root, text='File Open', command=filecallback).pack(fill=X)
 
-Button(text='Quit', command=callback).pack(fill=X)
-Button(text='Answer', command=answer).pack(fill=X)
+Button(root, 
+       text='Choose Color', 
+       fg="darkgreen", 
+       command=colorcallback).pack(fill=X)
+
+Button(text='Quit', 
+       command=root.quit,
+       fg="red").pack(fill=X)
+
 mainloop()
