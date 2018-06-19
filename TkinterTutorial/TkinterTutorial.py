e1894206d6fa169fc7525e46213cc40340b7ad84
bhang <bhang@yahoo.com>
2018-06-19 16:14:31 -0400

    grid layout

--- TkinterTutorial/TkinterTutorial.py
+++ TkinterTutorial/TkinterTutorial.py
@@ -23,4 +23,12 @@ helpmenu = Menu(menu)
 menu.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="About...", command=About)
 
+colours = ['red','green','orange','white','yellow','blue']
+
+r = 0
+for c in colours:
+    Label(text=c, relief=RIDGE,width=15).grid(row=r,column=0)
+    Entry(bg=c, relief=SUNKEN,width=10).grid(row=r,column=1)
+    r = r + 1
+
 mainloop()
